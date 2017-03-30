436eb339c0dc9843b75e1ad4424602c58e23be5c
Llenard Gonzalo <llenard.gonzalo@gmail.com>
2017-03-30 15:58:01 +0100

    v1.0

---
+++ Hellothere.java
@@ -0,0 +1,19 @@
+
+package hellothere;
+
+/**
+ *
+ * @author 14050239 - Llenard Gonzalo
+ */
+public class Hellothere {
+
+    /**
+     * @param args the command line arguments
+     * Most dankest, perfect java program ever made
+     */
+    public static void main(String[] args) {
+        System.out.println("Its Obi_wan Kenobi");
+        System.out.println("Hello there");
+    }
+    
+}
